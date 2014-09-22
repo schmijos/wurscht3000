c13420cb71b579e6221578cd0d8278bdc4880bfc
schmijos <jschmid@fastmail.net>
2014-09-22 22:22:42 +0200

    nicer rewrite of distance calculations

--- wurscht3000.js
+++ wurscht3000.js
@@ -1,74 +1,59 @@
 // Generates a base palette for cyclic additive HAM7
 function generateBasePalette() {
-    var arr = [];
+    return [
+        [ 31, 17, 13],
+        [ 13, 31, 17],
+        [ 17, 13, 31],
+        [240,240,240],
+    ];
+}
 
-    var r,g,b;
-    for(var i=0; i<1; i++) {
-        arr[arr.length] = [31,17,13];
-        arr[arr.length] = [13,31,17];
-        arr[arr.length] = [17,13,31];
-        arr[arr.length] = [240,240,240];
+// Calculate the nearest distance from the previously shown pixel to the next one
+function calcBestDiffIndex(prevEncodedColor, nextSourceColor, basePalette) {
+    var r,g,b; minDistance, minIndex;
+
+    // calculate distance between previously encoded frame and the next one to be encoded
+    r = (nextSourceColor[0] - prevEncodedColor[0]) % 256;
+    g = (nextSourceColor[1] - prevEncodedColor[1]) % 256;
+    b = (nextSourceColor[2] - prevEncodedColor[2]) % 256;
+        
+    for (i = 0; i < basePalette.length; i++) {
+        // calculate distance between diff and base palette
+        var dist = Math.pow(
+            Math.pow(basePalette[0] - r, 2) + 
+            Math.pow(basePalette[1] - g, 2) +
+            Math.pow(basePalette[2] - b, 2)
+        , 0.5); // Euclidian Distance
+
+        // keep the smallest distance
+        if (dist < minDistance) {
+            minDistance = dist;
+            minIndex = i;
+        }        
     }
-
-    return arr;
+    
+    return minIndex;
 }
 
-// Find nearest color by euclidian distance
-function findNearestColor(referenceColor, someColors) {
-    var rWeight = 1, // we could prefer specific color parts
-        gWeight = 1,
-        bWeight = 1,
-        minDist = Infinity,
-        foundColor;
-    someColors.forEach(function(q){
-            var dist = Math.pow(
-                    rWeight * Math.pow(referenceColor[0] - q[0], 2) 
-                    + gWeight * Math.pow(referenceColor[1] - q[1], 2)
-                    + bWeight * Math.pow(referenceColor[2] - q[2], 2)
-                    , 0.5); // Euclidian Distance
-            if (dist < minDist) {
-                minDist = dist;
-                foundColor = q;
-            }
-    });
-    return foundColor;
+// Calculate the jump diff to reach the next frame
+function calcNewDiffFrame(prevEncodedFrame, nextSourceFrame, basePalette) {
+    var diff = new Array(prevEncodedFrame.length);
+    for (i = 0; i < prevEncodedFrame.length; i++) {
+        diff[i] = calcBestDiffIndex(prevEncodedFrame[i], prevSourceColor[i], basePalette);
+    }
+    return diff;
 }
 
-// Calculate all valid next HAM7 colors (by jump with addition)
-function calculateJumpCandidates(basePalette, previousColor) {
-    var result = [];
-    var r,g,b;
-    basePalette.forEach(function(baseColor){
-            r = (previousColor[0] + baseColor[0]) % 256;
-            g = (previousColor[1] + baseColor[1]) % 256;
-            b = (previousColor[2] + baseColor[2]) % 256;
-            //result[result.length] = [ previousColor[0], g, b ]; // hold red
-            //result[result.length] = [ r, previousColor[1], b ]; // hold green
-            //result[result.length] = [ r, g, previousColor[2] ]; // hold blue
-            result[result.length] = [ r,g,b];
-    });
-    return result;
-}
-
-// Get the HAM representation of a source color using the previous HAM color and the base palette.
-function convertToHam(sourceColor, previousHamColor, basePalette) {
-    var indexColor = findNearestColor(sourceColor, basePalette);
-    var jumpCandidates = calculateJumpCandidates(basePalette, previousHamColor);
-    //jumpCandidates[jumpCandidates.length] = indexColor; // indclude index color into calculations
-    return findNearestColor(sourceColor, jumpCandidates); // return HAM Color
-}
-
-// Convert a color array to the correspondinnnng HAM representation
-function convertManyToHam(sourceColors, basePalette) {
-    var result = [];
-    var previousHamColor = [128, 128, 128]; // init with cube center
-    var hamColor;
-    sourceColors.forEach(function(sourceColor){
-            hamColor = convertToHam(sourceColor, previousHamColor, basePalette);
-            previousHamColor = hamColor;
-            result[result.length] = hamColor;
-    });
-    return result;
+// Render pixel by pixel on a canvas context
+function renderFrame(ctx, diff, width, height) {
+    imageData = ctx.getImageData(0, 0, width, height);
+    for (i = 0; i < imageData.data.length;) {
+        imageData.data[i] = (imageData.data[i] + diff[i++]) % 256; // Red
+        imageData.data[i] = (imageData.data[i] + diff[i++]) % 256; // Green
+        imageData.data[i] = (imageData.data[i] + diff[i++]) % 256; // Blue
+        imageData.data[i] = imageData.data[i++]; // Alpha
+    }
+    ctx.putImageData(imageData, 0, 0);
 }
 
 // DOM READY - GUI Tests:
@@ -77,17 +62,6 @@ function convertManyToHam(sourceColors, basePalette) {
     var black = [0,   0,   0  ];
     var white = [255, 255, 255];
     var basePalette = generateBasePalette();
-
-    function renderFrame(ctx, diff, width, height) {
-        imageData = ctx.getImageData(0, 0, width, height);
-        for (i = 0; i < imageData.data.length;) {
-            imageData.data[i] = (imageData.data[i] + diff[i++]) % 256; // Red
-            imageData.data[i] = (imageData.data[i] + diff[i++]) % 256; // Green
-            imageData.data[i] = (imageData.data[i] + diff[i++]) % 256; // Blue
-            imageData.data[i] = imageData.data[i++]; // Alpha
-        }
-        ctx.putImageData(imageData, 0, 0);
-    }
 
     // Image sample
     function imageLoaded(ev) {
