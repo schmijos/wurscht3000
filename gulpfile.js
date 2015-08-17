2bdd1ac75b06d5982cf4a51ae4e947565bdef55d
schmijos <jschmid@fastmail.net>
2015-08-17 21:18:09 +0200

    separated compilation of lib and specs

--- gulpfile.js
+++ gulpfile.js
@@ -4,21 +4,29 @@ var gulp = require('gulp');
 var jasmine = require('gulp-jasmine');
 var ts = require('gulp-typescript');
 
-gulp.task('ts', function () {
-	var tsProject = ts.createProject('tsconfig.json');
-  var tsResult = tsProject.src()
-    .pipe(ts({
-        noImplicitAny: true,
-        out: 'output.js'
-      }));
+gulp.task('ts_test', function () {
+	var tsProject = ts.createProject('tsconfig.json', {
+			noImplicitAny: true,
+			out: 'test.js'
+		});
+  var tsResult = gulp.src('src/**/*.ts').pipe(ts(tsProject)); // also include spec files
   return tsResult.js.pipe(gulp.dest('built/local'));
 });
 
+gulp.task('ts', function () {
+	var tsProject = ts.createProject('tsconfig.json', {
+			noImplicitAny: true,
+			out: 'dist.js'
+		});
+  var tsResult = tsProject.src().pipe(ts(tsProject));
+  return tsResult.js.pipe(gulp.dest('built/local'));
+});
 
 // Test JS
-gulp.task('test', function () {
-	return gulp.src('built/local/**/*_spec.js')
+gulp.task('test', ['ts_test'], function () {
+	return gulp.src('built/local/test.js')
 					     .pipe(jasmine());
 });
 
-gulp.task('default', ['ts']);
+gulp.task('default', ['test']);
+gulp.task('build', ['ts']);
